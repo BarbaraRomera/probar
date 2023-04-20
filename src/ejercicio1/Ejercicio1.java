5a9719646e55a46a29606e9e2ac20e8f85577d54
barbara romera <barbiinoemii@gmail.com>
2023-04-20 17:07:13 -0300

    segundo commit

--- src/ejercicio1/Ejercicio1.java
+++ src/ejercicio1/Ejercicio1.java
@@ -19,13 +19,15 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner leer= new Scanner(System.in);   
        int num1,num2,suma;
+       int nombre;
         System.out.println("ingrese el primer numero:");
      num1= leer.nextInt();
         System.out.println("ingrese el segundo numero");
      num2= leer.nextInt();
      suma=num2+num1;
+     nombre=5;
       System.out.println("el resultado del "+ num1 +"+" +num2+ " = "+suma);
-     
+        System.out.println("nombre ="+nombre);
 // TODO code application logic here
     }
     
